463a3ee7a3386d9e612ef8d85a48367b68107d47
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-02-10 16:28:43 -0500

    Code cleanup and draw call abstraction

--- GPR440/Assets/AI/ContextMaps/Editor/IContextProviderEditor.cs
+++ GPR440/Assets/AI/ContextMaps/Editor/IContextProviderEditor.cs
@@ -15,17 +15,24 @@ public class IContextProviderEditor : Editor
 
     private RenderTexture preview;
     private bool showingPreview = false;
+    private Material lineDrawMat;
 
     private void OnEnable()
     {
         preview = new RenderTexture(PREVIEW_SIZE, PREVIEW_SIZE, 0);
         if (!preview.IsCreated()) preview.Create();
+
+        lineDrawMat = new Material(Shader.Find("Hidden/Internal-Colored"));
+        lineDrawMat.hideFlags = HideFlags.HideAndDontSave;
     }
 
     private void OnDisable()
     {
         preview.Release();
         preview = null;
+
+        DestroyImmediate(lineDrawMat);
+        lineDrawMat = null;
     }
 
     public override void OnInspectorGUI()
@@ -62,36 +69,32 @@ public class IContextProviderEditor : Editor
             GL.LoadOrtho();
 
             //Set up for rendering
-            Material mat = new Material(Shader.Find("Hidden/Internal-Colored"));
-            mat.hideFlags = HideFlags.HideAndDontSave;
             GL.Clear(true, true, Color.black);
-            mat.SetPass(0);
+            lineDrawMat.SetPass(0);
 
-            void Vertex(float x, float y) => GL.Vertex3(x/PREVIEW_SIZE, 1 - y/PREVIEW_SIZE, 0);
+            void VertexXY(float x, float y) => GL.Vertex3(x/PREVIEW_SIZE, 1 - y/PREVIEW_SIZE, 0);
+            const float QUARTER_TURN = Mathf.PI / 2;
+            void VertexRadial(float ang, float r) => VertexXY(Mathf.Cos(ang - QUARTER_TURN)*r + PREVIEW_SIZE/2, Mathf.Sin(ang - QUARTER_TURN)*r + PREVIEW_SIZE/2);
 
             //Draw agent needle
             GL.Begin(GL.LINE_STRIP);
             GL.Color(Color.white);
             const int needleRadius = 20;
-            Vertex(PREVIEW_SIZE/2 +  0                , PREVIEW_SIZE/2 + -     needleRadius);
-            Vertex(PREVIEW_SIZE/2 +  0.7f*needleRadius, PREVIEW_SIZE/2 +  0.7f*needleRadius);
-            Vertex(PREVIEW_SIZE/2 +  0                , PREVIEW_SIZE/2 +  0.3f*needleRadius);
-            Vertex(PREVIEW_SIZE/2 + -0.7f*needleRadius, PREVIEW_SIZE/2 +  0.7f*needleRadius);
-            Vertex(PREVIEW_SIZE/2 +  0                , PREVIEW_SIZE/2 + -     needleRadius);
+            VertexXY(PREVIEW_SIZE/2 +  0                , PREVIEW_SIZE/2 + -     needleRadius);
+            VertexXY(PREVIEW_SIZE/2 +  0.7f*needleRadius, PREVIEW_SIZE/2 +  0.7f*needleRadius);
+            VertexXY(PREVIEW_SIZE/2 +  0                , PREVIEW_SIZE/2 +  0.3f*needleRadius);
+            VertexXY(PREVIEW_SIZE/2 + -0.7f*needleRadius, PREVIEW_SIZE/2 +  0.7f*needleRadius);
+            VertexXY(PREVIEW_SIZE/2 +  0                , PREVIEW_SIZE/2 + -     needleRadius);
             GL.End();
 
             //Draw center circle
             GL.Begin(GL.LINE_STRIP);
             int nEntries = contextMap.entries.Length;
             const int centerRadius = 30;
-            const float QUARTER_TURN = Mathf.PI / 2;
             GL.Color(Color.white);
             for (int i = 0; i < nEntries+1; ++i)
             {
-                Vector2 pos = new Vector2(Mathf.Cos(Mathf.PI*2*i/nEntries - QUARTER_TURN), Mathf.Sin(Mathf.PI*2*i/nEntries - QUARTER_TURN));
-                pos *= centerRadius; //Scale up
-                pos += Vector2.one * PREVIEW_SIZE/2;
-                Vertex(pos.x, pos.y);
+                VertexRadial(Mathf.PI*2*i/nEntries, centerRadius);
             }
             GL.End();
 
